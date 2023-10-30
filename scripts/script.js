f58e036fa1d1510ea376aacce5f04365fca67c38
Houssam Benlagha <houssambenlagha0@gmail.com>
2023-10-30 17:00:22 +0100

    display location name done

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
 const apiKEY = '19195f675238682b98a2e612b103d1f1';
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather'
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 const weatherCondion = document.querySelector('.js-display-weather-condition');
 const degree = document.querySelector('.js-degree');
 const weatherIcon = document.querySelector('.js-weather-icon');
+const date = document.querySelector('.js-date');
+const locationTitle = document.querySelector('.js-current-location-text');
+const locationName = document.querySelector('.js-display-location-name');
 
 
 function fetchWeatherData(cityName){
@@ -21,6 +24,10 @@ function fetchWeatherData(cityName){
             weatherCondion.innerHTML = Data.weather[0].description;
             degree.innerHTML = `${Math.round(Data.main.temp)}°`;
             weatherIcon.src = `/img/weather-icon/${Data.weather[0].icon}.png`
+            date.innerHTML = getCurrentDate();
+            locationTitle.innerHTML = 'Location';
+            locationName.innerHTML = `${Data.name}, ${getCountryName(Data.sys.country)}`;
+
             return console.log(Data);
         })
         .catch((error) => {
@@ -46,6 +53,25 @@ searchBtn.addEventListener('click', (event) => {
 });
 
 
-function displayWeatherInfo(){
 
+
+
+function getCurrentDate(){
+    const d = new Date();
+
+    return `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`;
+}
+
+
+
+
+
+function getCountryName(countryCode) {
+    const regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+
+    console.log(regionNames.of(countryCode));
+
+    return regionNames.of(countryCode);
+
+    
 }
